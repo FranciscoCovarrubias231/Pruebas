749275077cb7c75f20b3c6c9967052998690b2ce
FranciscoCovarrubias231 <94408904+FranciscoCovarrubias231@users.noreply.github.com>
2021-11-15 16:29:37 -0600

    Update Prueba.java

--- src/Prueba.java
+++ src/Prueba.java
@@ -6,5 +6,6 @@ public class Prueba
        System.out.println("Hola");
         System.out.println("Hola de nuevo");
         System.out.println("Hola Master ¿Que paso?");
+        System.out.println("Nada carnal");
     }
 }
